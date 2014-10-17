085d2316be4fac4823a79c3f1f55a1591566aaa6
Michel Jouvin <jouvin@lal.in2p3.fr>
2014-10-17 17:57:02 +0200

    Add support for last svnkit version (1.8.6) - Required for TLS support (SSLv3 Poodle vulnerability) - svnkit 1.8.6 used by create-vanilla-SCDB.sh - svnkit 1.3.x no longer supported
    Fixes #28.

--- utils/scdb/create-vanilla-SCDB.sh
+++ utils/scdb/create-vanilla-SCDB.sh
@@ -18,7 +18,7 @@ scdb_external_list="ant panc scdb-ant-utils svnkit"
 panc_version=panc-10.1
 ant_version=apache-ant-1.7.1
 scdb_ant_utils_version=scdb-ant-utils-9.0.2
-svnkit_version=svnkit-1.3.5
+svnkit_version=svnkit-1.8.6
 
 # scdb source is typically a clone of GitHub scdb repo, switched to the appropriate
 # version/branch. By default, the root of the clone is 2 level upper than the directory
